addcc31eee8f239f1cabec658cb6689847e4f239
TadurShilpa <shilpa.tadur>
2023-05-17 13:43:11 +0530

    Expense Item commit

---
+++ ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem(){
+    return (
+    <div>
+    <h2> Expense items</h2>
+    <ul>
+        <li>Food Rs 10</li>
+        <li>Petrol Rs 100</li>
+        <li>Movies Rs 100</li>
+    </ul>
+    </div>
+    );
+}
+
+export default ExpenseItem;
